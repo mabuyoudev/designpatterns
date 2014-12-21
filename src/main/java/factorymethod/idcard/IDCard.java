e735c446be18308cd60ff54f94914753ce6a0600
mabuyoudev <mabu.you.dev@gmail.com>
2014-12-21 21:11:41 +0900

    設定変更

---
+++ src/main/java/factorymethod/idcard/IDCard.java
@@ -0,0 +1,23 @@
+package factorymethod.idcard;
+
+import factorymethod.framework.Product;
+
+public class IDCard extends Product {
+
+  private String owner;
+
+  IDCard(String owner) {
+    System.out.println(owner + "のカードを作ります。");
+    this.owner = owner;
+  }
+
+  @Override
+  public void use() {
+    System.out.println(owner + "のカードを使います。");
+  }
+
+  public String getOwner() {
+    return owner;
+  }
+
+}
